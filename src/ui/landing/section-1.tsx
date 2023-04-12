73bb96c7f879a4b1442f7b1083bc433415331080
Meraj Kazi <kazimeraj11@gmail.com>
2023-04-12 17:47:42 +0600

    hero section responsive

--- src/ui/landing/section-1.tsx
+++ src/ui/landing/section-1.tsx
@@ -41,7 +41,7 @@ export const SectionOne = () => {
         <object
           type="image/svg+xml"
           data="/HeroAnimationMobile.svg"
-          className="w-[140%] h-[auto] ml-[-50px] sm:hidden"
+          className="w-[140%] h-[auto] ml-[-50px] 360:ml-[-70px] 414:ml-[-80px] sm:hidden"
         ></object>
         ``
         <div
